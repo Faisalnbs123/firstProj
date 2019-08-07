f0f308b8d7455e97719e37a2a23a20894027b8ea
Faisal Ejaz <Faisal.Ejaz@Cdocs.local>
2019-08-07 13:20:41 +0500

    first-proj

---
+++ dockerize/src/main/java/com/example/dockerize/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.dockerize.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	@GetMapping(path="/emp") 
+	public int get()
+	{
+		return 5;
+	}
+}
